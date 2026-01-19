32b3d173cf98dc6f87ffd87db800c550e8c83969
Nikheal <n3xcode@gmail.com>
2026-01-19 11:20:38 +0200

    Initial Commit

---
+++ Recipe Book/Recipe_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_BookApp.swift
+//  Recipe Book
+//
+//  Created by Grizzowl on 2026/01/19.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_BookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
